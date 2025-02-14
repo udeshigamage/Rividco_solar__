9e69ee02a1faed560ed45cae2e2c8401f695deb8
udeshi <udeshigamage92@gmail.com>
2025-02-14 23:46:18 +0530

    Add project files.

---
+++ Rividco_solar__/Dbcontext/AppDbcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rividco_solar__.Models;
+using System;
+
+namespace Rividco_solar__.Dbcontext
+{
+    public class AppDbcontext : DbContext 
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Vendor> Vendor { get; set; }
+    }
+}
---
+++ Rividco_solar__/Models/Vendor.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rividco_solar__.Models
+{
+    public class Vendor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Vendor_ID { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string mobileno { get; set; }
+
+        public string officeno { get; set; }
+
+        public string comment { get; set; }
+
+        public string Address { get; set; }
+
+        public string category { get; set; }
+
+        public string Company_ID { get; set; }
+
+
+
+    }
+}
